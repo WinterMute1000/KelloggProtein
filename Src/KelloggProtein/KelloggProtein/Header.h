73343339858fa134145d84b32eb5bd041c8a228d
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2019-08-26 20:56:11 +0900

    Header
    Making Header(Adjust code  that I got  from google)

---
+++ Src/KelloggProtein/KelloggProtein/Header.h
@@ -0,0 +1,225 @@
+#pragma once
+#ifndef _HEADER_H__
+#include "windows.h"
+#include "stdio.h"
+#include "tlhelp32.h"
+
+typedef DWORD(WINAPI* PFNTCREATETHREADEX)
+(
+	PHANDLE                 ThreadHandle,
+	ACCESS_MASK             DesiredAccess,
+	LPVOID                  ObjectAttributes,
+	HANDLE                  ProcessHandle,
+	LPTHREAD_START_ROUTINE  lpStartAddress,
+	LPVOID                  lpParameter,
+	BOOL	                CreateSuspended,
+	DWORD                   dwStackSize,
+	DWORD                   dw1,
+	DWORD                   dw2,
+	LPVOID                  Unknown
+);
+
+enum { INJECTION_MODE = 0, EJECTION_MODE };
+
+BOOL SetPrivilege(LPCTSTR lpszPrivilege, BOOL bEnablePrivilege)
+{
+	TOKEN_PRIVILEGES tp;
+	HANDLE hToken;
+	LUID luid;
+
+	if (!OpenProcessToken(GetCurrentProcess(),
+		TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,
+		&hToken))
+	{
+		printf("OpenProcessToken error: %u\n", GetLastError());
+		return FALSE;
+	}
+
+	if (!LookupPrivilegeValue(NULL,            // lookup privilege on local system
+		lpszPrivilege,   // privilege to lookup 
+		&luid))        // receives LUID of privilege
+	{
+		printf("LookupPrivilegeValue error: %u\n", GetLastError());
+		return FALSE;
+	}
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = luid;
+	if (bEnablePrivilege)
+		tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	else
+		tp.Privileges[0].Attributes = 0;
+
+	// Enable the privilege or disable all privileges.
+	if (!AdjustTokenPrivileges(hToken,
+		FALSE,
+		&tp,
+		sizeof(TOKEN_PRIVILEGES),
+		(PTOKEN_PRIVILEGES)NULL,
+		(PDWORD)NULL))
+	{
+		printf("AdjustTokenPrivileges error: %u\n", GetLastError());
+		return FALSE;
+	}
+
+	if (GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+	{
+		printf("The token does not have the specified privilege. \n");
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+BOOL InjectDll(DWORD dwPID, LPCTSTR szDllPath)
+{
+	HANDLE                  hProcess, hThread;
+	LPVOID                  pRemoteBuf;
+	DWORD                   dwBufSize = lstrlen(szDllPath) + 1;
+	LPTHREAD_START_ROUTINE  pThreadProc;
+
+	if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID)))
+	{
+		printf("OpenProcess(%d) failed!!!\n", dwPID);
+		return FALSE;
+	}
+
+	pRemoteBuf = VirtualAllocEx(hProcess, NULL, dwBufSize,
+		MEM_COMMIT, PAGE_READWRITE);
+
+	WriteProcessMemory(hProcess, pRemoteBuf,
+		(LPVOID)szDllPath, dwBufSize, NULL);
+
+	pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("kernel32.dll"),"LoadLibraryA");
+#if defined(_WIN64)
+	FARPROC     pFunc = NULL;
+	pFunc = GetProcAddress(GetModuleHandle("ntdll.dll"), "NtCreateThreadEx");
+	if (pFunc == NULL)
+	{
+		printf(" GetProcAddress(\"NtCreateThreadEx\") failed!!! [%d]\n",
+			GetLastError());
+		return FALSE;
+	}
+	((PFNTCREATETHREADEX)pFunc)(&hThread,
+		0x1FFFFF,
+		NULL,
+		hProcess,
+		pThreadProc,
+		pRemoteBuf,
+		FALSE,
+		NULL,
+		NULL,
+		NULL,
+		NULL);
+#else
+	hThread = CreateRemoteThread(hProcess, NULL, 0, pThreadProc, pRemoteBuf, 0, NULL);
+#endif
+	WaitForSingleObject(hThread, INFINITE);
+
+	VirtualFreeEx(hProcess, pRemoteBuf, 0, MEM_RELEASE);
+
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+
+	return TRUE;
+}
+
+BOOL EjectDll(DWORD dwPID, LPCTSTR szDllPath)
+{
+	BOOL                    bMore = FALSE, bFound = FALSE;
+	HANDLE                  hSnapshot, hProcess, hThread;
+	MODULEENTRY32           me = { sizeof(me) };
+	LPTHREAD_START_ROUTINE  pThreadProc;
+
+	if (INVALID_HANDLE_VALUE ==
+		(hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID)))
+		return FALSE;
+
+	bMore = Module32First(hSnapshot, &me);
+	for (; bMore; bMore = Module32Next(hSnapshot, &me))
+	{
+		if (!_stricmp(me.szModule, szDllPath) ||
+			!_stricmp(me.szExePath, szDllPath))
+		{
+			bFound = TRUE;
+			break;
+		}
+	}
+
+	if (!bFound)
+	{
+		CloseHandle(hSnapshot);
+		return FALSE;
+	}
+
+	if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID)))
+	{
+		printf("OpenProcess(%d) failed!!!\n", dwPID);
+		CloseHandle(hSnapshot);
+		return FALSE;
+	}
+
+	pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("kernel32.dll"),"FreeLibrary");
+#if defined(_WIN64)
+		FARPROC     pFunc = NULL;
+	pFunc = GetProcAddress(GetModuleHandle("ntdll.dll"), "NtCreateThreadEx");
+	if (pFunc == NULL)
+	{
+		printf(" GetProcAddress(\"NtCreateThreadEx\") failed!!! [%d]\n",
+			GetLastError());
+		return FALSE;
+	}
+	((PFNTCREATETHREADEX)pFunc)(&hThread,
+		0x1FFFFF,
+		NULL,
+		hProcess,
+		pThreadProc,
+		me.modBaseAddr,
+		FALSE,
+		NULL,
+		NULL,
+		NULL,
+		NULL);
+#else
+	hThread = CreateRemoteThread(hProcess, NULL, 0, pThreadProc, me.modBaseAddr, 0, NULL);
+#endif
+	WaitForSingleObject(hThread, INFINITE);
+
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+	CloseHandle(hSnapshot);
+
+	return TRUE;
+}
+
+BOOL InjectAllProcess(int nMode, LPCTSTR szDllPath)
+{
+	DWORD                   dwPID = 0;
+	HANDLE                  hSnapShot = INVALID_HANDLE_VALUE;
+	PROCESSENTRY32          pe;
+
+	// Get the snapshot of the system
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+
+	// find process
+	Process32First(hSnapShot, &pe);
+	do
+	{
+		dwPID = pe.th32ProcessID;
+
+		if (dwPID < 100)
+			continue;
+
+		if (nMode == INJECTION_MODE)
+			InjectDll(dwPID, szDllPath);
+		else
+			EjectDll(dwPID, szDllPath);
+	} while (Process32Next(hSnapShot, &pe));
+
+	CloseHandle(hSnapShot);
+
+	return TRUE;
+}
+
+#endif
